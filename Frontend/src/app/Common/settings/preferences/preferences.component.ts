63143a139ec4b8f2dc19d5c4df4b29bcaa525965
ayaatef11 <ayabadrin667@gmail.com>
2026-05-07 00:30:56 +0300

    apply dark theme

--- Frontend/src/app/Common/settings/preferences/preferences.component.ts
+++ Frontend/src/app/Common/settings/preferences/preferences.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-preferences',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './preferences.component.html',
   styleUrl: './preferences.component.css'
 })
 export class PreferencesComponent {
+  constructor(){}
+  ngOnInit() {
+  const savedTheme = localStorage.getItem('theme')??'light';
+this.selectedTheme=savedTheme;
+  if (savedTheme === 'dark') {
+    this.isDark = true;
+    document.body.classList.add('dark-theme');
+  } 
+    else if (savedTheme === 'light') {
+    this.isDark = false;
+    document.body.classList.remove('dark-theme');
+  } 
+  else {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+    this.isDark = prefersDark;
+    document.body.classList.toggle('dark-theme', prefersDark);
+  }
+}
+//***********************VARIABLES**************** */
+isDark = false;
+selectedTheme: string = 'light';
+
+///*****************FUNCTIONS************* */
+onThemeChange(event: any) {
+  const value = event.target.value;
+
+  if (value === 'dark') {
+    this.isDark = true;
+    document.body.classList.add('dark-theme');
+    localStorage.setItem('theme', 'dark');
+  } 
+  else if (value === 'light') {
+    this.isDark = false;
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
+  }
+  else {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    this.isDark = prefersDark;
+
+    document.body.classList.toggle('dark-theme', prefersDark);
+
+    localStorage.setItem('theme', 'auto');
+  }
+}
 }
